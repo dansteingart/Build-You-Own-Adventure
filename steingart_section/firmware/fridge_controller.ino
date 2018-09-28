d8a8548142018e9fb274dc7d8779622a12119fc9
Dan Steingart <steingart@princeton.edu>
2018-09-28 16:17:59 -0400

    Update fridge_controller.ino

--- steingart_section/firmware/fridge_controller.ino
+++ steingart_section/firmware/fridge_controller.ino
@@ -21,7 +21,9 @@ unsigned long sendTime;
 void setup() {
     Particle.function("set_state", set_state);
     sendTime = millis();
-    for (int i = 3; i<7;i++) pinMode(i,OUTPUT);
+    for (int i = 3; i<8;i++) pinMode(i,OUTPUT);
+    RGB.control(true);
+    RGB.color(255,200,50);
 
 }
 
@@ -31,11 +33,14 @@ void loop()
     if (millis() - sendTime > 1000)
     {
 
+      digitalWrite(7,HIGH);
       float TI = TC.readInternal();
       float TO = TC.readCelsius();
       sprintf(publishString,"{\"state\":%d,\"TI\":%f,\"TO\":%f}",state,TI,TO);
       Particle.publish("MAE_519_LAB_1",publishString);
       sendTime = millis();
+      delay(50);
+      digitalWrite(7,LOW);
     }
 }
 
@@ -43,6 +48,7 @@ void loop()
 void off()
 {
     for (int i = 3; i < 7; i++) digitalWrite(i,LOW);
+    RGB.color(255,200,50);
     delay(200);
 }
 
@@ -51,6 +57,8 @@ void hot()
     off();
     digitalWrite(3,HIGH);
     digitalWrite(5,HIGH);
+    RGB.color(255,0,0);
+
 }
 
 void cold()
@@ -58,6 +66,8 @@ void cold()
     off();
     digitalWrite(4,HIGH);
     digitalWrite(6,HIGH);
+    RGB.color(0,0,255);
+
 }
 
 void stater(int i)
@@ -65,6 +75,7 @@ void stater(int i)
     if (i == 1)    hot();
     else if (i==2) cold();
     else           off();
+    state = i;
 }
 
 int set_state(String potter)
